3686d35c8f4658ed977d6fe91ef0bb35d1caec96
jwyjohn <jwyjohn@foxmail.com>
2021-02-12 13:38:24 +0800

    Changed file database to GUN DBM.

--- app/api.py
+++ app/api.py
@@ -2,52 +2,31 @@
 
 from flask import Flask
 import flask_restful as restful
-from flask_sqlalchemy import SQLAlchemy
+import hashlib
+import dbm
 
 
+with dbm.open('/data/example.db', 'n') as db:
+    db['key'] = 'value'
+    db['today'] = 'Sunday'
+    db['author'] = 'Doug'
+
+print(dbm.whichdb('/data/example.db'))
 
 
 
 app = Flask(__name__)
 api = restful.Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/test.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=False)
-    email = db.Column(db.String(120), unique=False)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-db.create_all()
-admin = User('admin', 'admin@example.com')
-guest = User('guest', 'guest@example.com')
-db.session.add(admin)
-db.session.add(guest)
-db.session.commit()
-
-
-
-
-
-
-
 
 
 
 class HelloWorld(restful.Resource):
     def get(self):
         #return {'hello': 'world'}
-        return str(User.query.all())
+        with dbm.open('/data/example.db', 'r') as db:
+            return str(db.keys())
 
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    #app.run(debug=True)
     app.run(debug=True, host='0.0.0.0')
