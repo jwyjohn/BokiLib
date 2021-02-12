56045aa552c05e1e74f26cc2fb7dd8a1ecc4621f
jwyjohn <jwyjohn@foxmail.com>
2021-02-12 09:00:03 +0800

    Modified rename.sh

--- utils/gen_testdata.sh
+++ utils/gen_testdata.sh
@@ -3,13 +3,18 @@
 export LANG=C.UTF-8
 export DOUJIN_DIR=$PWD
 
-mkdir $DOUJIN_DIR/tmp/doujin{001..100}
+#mkdir $DOUJIN_DIR/tmp/doujin{00001..99999}
+for i in {00000..99999};
+do
+        mkdir -v "./tmp/doujin$i"
+done
+
 cd $DOUJIN_DIR/tmp
 
 for doujin in `ls`;
 do
 	cd $DOUJIN_DIR/tmp/$doujin
-	for i in {0..9};do dd if=/dev/urandom of=$i.img bs=1M count=1 iflag=fullblock;done
+	for i in {0..9};do dd if=/dev/urandom of=$i.img bs=1K count=1 iflag=fullblock;done
 	pwd
 	ls
 
