e64b56d20518b9a6a107ff84a6574dfff64d2f1b
jwyjohn <jwyjohn@foxmail.com>
2021-02-12 10:19:32 +0800

    Tested sql model.

--- app/api.py
+++ app/api.py
@@ -1,17 +1,50 @@
 # -*- encoding: utf-8 -*-
 
-import sqlite3
 from flask import Flask
 import flask_restful as restful
+from flask_sqlalchemy import SQLAlchemy
 
-DATABASE = ''
+
+
+
 
 app = Flask(__name__)
 api = restful.Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False)
+    email = db.Column(db.String(120), unique=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
+admin = User('admin', 'admin@example.com')
+guest = User('guest', 'guest@example.com')
+db.session.add(admin)
+db.session.add(guest)
+db.session.commit()
+
+
+
+
+
+
+
+
+
 
 class HelloWorld(restful.Resource):
     def get(self):
-        return {'hello': 'world'}
+        #return {'hello': 'world'}
+        return str(User.query.all())
 
 api.add_resource(HelloWorld, '/')
 
