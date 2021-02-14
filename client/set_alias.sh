d6801c1856c38e19a082e9d871769042d331a795
jwyjohn <jwyjohn@foxmail.com>
2021-02-14 18:45:52 +0800

    Added client shell scripts.

---
+++ client/set_alias.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+export LANG=C.UTF-8
+TMPDIR=./tmp
+
+for i in `ls $TMPDIR`;  
+do   
+	echo Taring: $i ...;  
+	tar -czvf "./tmp/$i.tar.gz" "./tmp/$i" && rm -rf "./tmp/$i" &
+done   
+wait
