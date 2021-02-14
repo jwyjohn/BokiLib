52d420cb41eb20fff09837e370a23da1c960743f
jwyjohn <jwyjohn@foxmail.com>
2021-02-14 09:25:02 +0800

    testing..

--- app/boki_db.py
+++ app/boki_db.py
@@ -18,7 +18,7 @@ def init_db(DATABASE_PATH='/data'):
             db['a0b65939670bc2c010f4d5d6a0b3e4e4590fb92b']=str(time.mktime(datetime.datetime.now().timetuple()))
 
     if not os.path.exists(DATABASE_PATH+'/filesize.dbm'):
-        with dbm.open(DATABASE_PATH+'/filedate.dbm', 'n') as db:
+        with dbm.open(DATABASE_PATH+'/filesize.dbm', 'n') as db:
             db['a0b65939670bc2c010f4d5d6a0b3e4e4590fb92b']='13'
     return 0
 
@@ -69,3 +69,40 @@ class BokiFileDB():
             except:
                 return 'No such file.'
         return res_date
+
+    def hash2size(self, sha1):
+        res_size=0
+        try:
+            assert(check_sha1(sha1))
+        except:
+            return "Invalid hash."
+        with dbm.open(self.__DBPATH+'/filesize.dbm', 'r') as sdb:
+            try:
+                res_size=int(sdb[sha1])
+            except:
+                return 'No such file.'
+        return res_size
+
+    def addfile(self, rawfile):
+        try:
+            assert type(rawfile)==type(b'A')
+            fsha1=hashlib.sha1(rawfile).hexdigest()
+            ftime=str(time.mktime(datetime.datetime.now().timetuple()))
+            fsize=str(len(rawfile))
+            with dbm.open(self.__DBPATH+'/rawfile.dbm', 'r') as fdb:
+                try:
+                    fdb[fsha1]=rawfile
+                except:
+                    return 'Invalid FileDB'
+            with dbm.open(self.__DBPATH+'/filedate.dbm', 'r') as ddb:
+                try:
+                    ddb[fsha1]=ftime
+                except:
+                    return 'Invalid Date'
+            with dbm.open(self.__DBPATH+'/filesize.dbm', 'r') as sdb:
+                try:
+                    sdb[fsha1]=fsize
+                except:
+                    return 'Invalid Size'
+        except:
+            return 'Invalid File'
