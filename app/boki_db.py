1f6c6d48820a13abf9b9ec3c047c9bef4f2e93a0
jwyjohn <jwyjohn@foxmail.com>
2021-02-14 08:33:54 +0800

    Added database src

---
+++ app/boki_db.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+
+import hashlib
+import dbm
+from io import BytesIO
+import os
+import datetime
+
+#init database
+
+if not os.path.exists('/data/rawfile.dbm'):
+    with dbm.open('/data/rawfile.dbm', 'n') as db:
+        db['a0b65939670bc2c010f4d5d6a0b3e4e4590fb92b']='Hello World!\n'
+
+if not os.path.exists('/data/filedate.dbm'):
+    with dbm.open('/data/filedate.dbm', 'n') as db:
+        db['a0b65939670bc2c010f4d5d6a0b3e4e4590fb92b']=str(datetime.datetime.now())
+
+if not os.path.exists('/data/filesize.dbm'):
+    with dbm.open('/data/filedate.dbm', 'n') as db:
+        db['a0b65939670bc2c010f4d5d6a0b3e4e4590fb92b']='13'
+
+class GetFileTime(restful.Resource):
+    def get(self, sha1):
+        res=''
+        with dbm.open('/data/filedate.dbm', 'r') as db:
+            try:
+                res=db[sha1].decode(encoding="utf-8", errors="strict")
+            except:
+                return 'No such file.'
+
+        return {'filedate':str(res)}
+
+class GetFile(restful.Resource):
+    def get(self, sha1):
+        res_file=b''
+        res_filename=str(datetime.datetime.now())
+        with dbm.open('/data/rawfile.dbm', 'r') as db:
+            try:
+                res_file=db[sha1]
+            except:
+                return 'No such file.'
+        b = BytesIO(res_file)
+        response = Response(b, content_type='application/octet-stream')
+        response.headers["Content-disposition"] = 'attachment; filename=%s' % res_filename  
+        return response
+
+
+class BokiFileDB():
+    """
+    Database to store file in bokilib.
+    Structure:  <hash>:<file bytes>
+
+    """
+    
